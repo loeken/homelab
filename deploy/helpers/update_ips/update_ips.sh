3da0ee2f18c1246cec76f3f3030ec3df2310abdc
loeken <loeken@internetz.me>
2022-05-01 10:31:34 +0200

    helper script to update k3s's external ip

---
+++ deploy/helpers/update_ips/update_ips.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+newip=`curl ifconfig.co -4`
+oldip=`cat /tmp/oldip`
+if [ "$oldip" == "" ]; then
+  echo $newip > /tmp/oldip
+fi
+if [ "$oldip" != "$newip" ]; then
+  sed -i "s/$oldip/$newip/g" /etc/systemd/system/k3s.service
+  systemctl daemon-reload
+  systemctl restart k3s
+  echo $newip > /tmp/oldip
+fi
